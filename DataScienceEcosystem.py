a91a1a5b9a11453b0397b782c6815b5571b36180
Segli96 <136557248+Segli96@users.noreply.github.com>
2023-06-13 23:44:01 -0600

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science and Tools Ecosystem
+# ---
+
+# ## Author ##
+#  Spencer Egli
+
+# ### Data Science tools and Ecosystem are summarized in this notebook.
+
+# **Objectives:**
+# - Popular Coding Languages in Data Science
+# - Libraries in Data Science
+# - Examples in Python
+
+# #### Some Popular coding Languages Data Scientist use are
+# ----------------------------
+
+# 1. Python
+# 2. C/C++
+# 3. SQL
+# 4. R
+# 5. Julia
+# 6. JavaScript
+# 7. SAS
+# 
+
+# #### Some commonly used Libraries in Data Science are 
+# ----
+
+# 1. TensorFlow
+# 2. NumPy
+# 3. Pandas
+
+# | Data Science Tools |
+# |---|
+# | Rstudio |
+# | Apache Spark |
+# | Tensorflow |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# ---
+
+# In[3]:
+
+
+#This simple arithmetic to multiple then add integers
+
+(3*4)+5
+
+
+# In[11]:
+
+
+#This will convert 200 minutes to hours by dividing by 60
+
+min=int(input("how many minutes?"))
+print(round(min/60, 2), "Hour(s)")
+
+
+# In[ ]:
+
+
+
+
